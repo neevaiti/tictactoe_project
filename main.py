f26f53f49d24cec9b05a300eded759613ef76a67
Ant <antonin.lemullois@pm.me>
2023-01-11 21:39:30 +0100

    Ajout en cours de la première fonction, qui définit la grille.

--- main.py
+++ main.py
@@ -3,6 +3,27 @@
 1.5 - Affichage de l'interface
 2 - Joueur qui joue
 3 - Autre Joueur qui joue
-4 - Vérifier que les joueur ne jouent pas sur l'autre
-5 - Si le joueur
+4 - Vérifier que les joueurs ne jouent pas sur l'autre
+5 - Si un des joueurs fait une ligne de 3 le jeu s'arrête et le joueur gagne
+6 - Si jamais aucun des joueurs n'aligne 3 objets alors la partie se termine sur un match nul
 """
+
+# Définissons en premier les limites de la grille
+
+grille = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+]
+
+def display_grille(grille:list) -> str:
+    """
+    Fonction qui prend une liste et qui va devoir décomposer cette liste pour en faire une grille.
+    :param grille: La liste contenant la grille.
+    :return: La grille qui apparaît dans la console.
+    """
+    for i in grille:
+       result = print(i)
+    return result
+
+print(display_grille(grille))
