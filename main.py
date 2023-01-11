97cccfdd959226370dad45c4cd94ba60048c927f
Ant <antonin.lemullois@pm.me>
2023-01-11 20:48:54 +0100

    Annonce du projet par étape.

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+1 - Définir la grille
+1.5 - Affichage de l'interface
+2 - Joueur qui joue
+3 - Autre Joueur qui joue
+4 - Vérifier que les joueur ne jouent pas sur l'autre
+5 - Si le joueur
+"""
