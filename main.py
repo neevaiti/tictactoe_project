301be9b352c9da494a32134e542a4acdffd636fb
Ant <antonin.lemullois@pm.me>
2023-01-12 10:54:43 +0100

    Création de la grille terminée. Maintenant il faut vérifier si la grille est remplie.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 """
-1 - Définir la grille
-1.5 - Affichage de l'interface
-2 - Joueur qui joue
-3 - Autre Joueur qui joue
-4 - Vérifier que les joueurs ne jouent pas sur l'autre
-5 - Si un des joueurs fait une ligne de 3 le jeu s'arrête et le joueur gagne
-6 - Si jamais aucun des joueurs n'aligne 3 objets alors la partie se termine sur un match nul
+1 - Définir la grille - ✅
+1.5 - Affichage de l'interface - ✅
+2 - Vérification de la grille - ⏳
+3 - Joueur qui joue - ⏳
+4 - Autre Joueur qui joue - ⏳
+5 - Vérifier que les joueurs ne jouent pas sur l'autre - ⏳
+6 - Si un des joueurs fait une ligne de 3 le jeu s'arrête et le joueur en question gagne - ⏳
+7 - Si aucun des joueurs n'aligne 3 objets alors la partie se termine sur un match nul - ⏳
 """
 
-# Définissons en premier les limites de la grille
+# Définition de la grille
 
 grille = [
     ["-", "-", "-"],
@@ -16,14 +17,40 @@ grille = [
     ["-", "-", "-"],
 ]
 
+player_cross = "X"
+player_tac = "O"
+
+# Mise en place des lignes de ma grille
 def display_grille(grille:list) -> str:
     """
     Fonction qui prend une liste et qui va devoir décomposer cette liste pour en faire une grille.
     :param grille: La liste contenant la grille.
     :return: La grille qui apparaît dans la console.
     """
-    for i in grille:
-       result = print(i)
-    return result
+    for space in grille:
+        print(" ".join(space))
 
-print(display_grille(grille))
+display_grille(grille)
+
+
+def check_grille(grille:list) -> str or bool:
+    """
+    Fonction qui va vérifier si la grille est remplie.
+    :param grille: La liste contenant la grille.
+    :return: Une vérification de si la liste est complétée par les joueurs et empêchant les joueurs de jouer l'un sur l'autre.
+    """
+    for space in grille:
+        if space is ['X', 'X', 'X']:
+            return False
+        elif space is ['O', '0', 'O']:
+            return False
+
+    for space in grille:
+        if space[0][0:2] is player_cross or player_tac:
+            print("Vous ne pouvez pas jouer sur la case d'un autre joueur")
+
+        elif space[1][0:2] is player_cross or player_tac:
+            print("Vous ne pouvez pas jouer sur la case d'un autre joueur")
+
+        elif space[2][0:2] is player_cross or player_tac:
+            print("Vous ne pouvez pas jouer sur la case d'un autre joueur")
